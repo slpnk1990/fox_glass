6403d8b4ff758bc8a8d124b40aea7010e1f31999
slpnk1990 <dimastroi@tut.by>
2022-06-06 11:44:55 +0300

    main

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY DATABASE_URL'] = 'postgres://postgres:1312@localhost/fox_glass'
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+    
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+db.create_all()
 
 
 @app.route('/')
